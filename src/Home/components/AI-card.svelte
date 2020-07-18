2e0defe070994a23e4536275de971669dccaf9a2
Dmitry <donveds@gmail.com>
2020-07-18 23:24:10 +0300

    Edited AI Card

--- src/Home/components/AI-card.svelte
+++ src/Home/components/AI-card.svelte
@@ -3,16 +3,35 @@
 </script>
 
 <div class="AICard">
-	<h3>Winter is comming</h3>
-  <p>Dont forget to check last day of fertilizetion</p>
+  <div class="AICard-message">
+    <h3>Winter is comming</h3>
+    <p>Dont forget to check last day of fertilizetion</p>
+  </div>
+  <div class="AICard-logo">
+    <img src="images/AI.png" alt="AI item">
+  </div>
 </div>
 
 <style>
 	.AICard {
-    background-color: khaki;
+    background-color: #cafcf6;
     width: 80%;
     border-radius: 30px;
     padding: 10px; 
     margin-bottom: 2.5%;
+    display: flex;
   }
+
+  .AICard-message {
+    flex-grow: 10;
+  }
+
+  .AICard-logo {
+    width: 100px;
+  }
+
+  .AICard-logo img {
+    height: 100px;
+  }
+
 </style>
